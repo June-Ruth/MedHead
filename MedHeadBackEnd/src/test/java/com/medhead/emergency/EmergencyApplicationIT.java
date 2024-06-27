148dc70e28556eab5ab37a5f5e27da4731639194
June-Ruth <n-barral@hotmail.fr>
2024-06-27 13:47:08 +0200

    add docker configuration

--- MedHeadBackEnd/src/test/java/com/medhead/emergency/EmergencyApplicationIT.java
+++ MedHeadBackEnd/src/test/java/com/medhead/emergency/EmergencyApplicationIT.java
@@ -5,11 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
